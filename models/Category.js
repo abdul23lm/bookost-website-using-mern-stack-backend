b7ce1841581ff5865a6e5a014a1f49ff0f0a7632
abdul23lm <abdul23lm@gmail.com>
2020-11-01 04:46:44 +0700

    Add required validation on schema

--- models/Category.js
+++ models/Category.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 
-const imageSchema = new mongoose.Schema({
-    imageUrl: {
-        type: String
+const categorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
     }
 })
 
-module.exports = mongoose.model('Image', imageSchema)
+module.exports = mongoose.model('Category', categorySchema)
--- models/Image.js
+++ models/Image.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 
-const categorySchema = new mongoose.Schema({
-    name: {
-        type: String
+const imageSchema = new mongoose.Schema({
+    imageUrl: {
+        type: String,
+        required: true
     }
 })
 
-module.exports = mongoose.model('Category', categorySchema)
+module.exports = mongoose.model('Image', imageSchema)

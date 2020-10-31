6e768a6bb5dca20973b79a9dc8a7693af5a66384
abdul23lm <abdul23lm@gmail.com>
2020-11-01 05:23:20 +0700

    Integrating sb-admin2 view and add nodemoon packages

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/sb-admin-2', express.static(path.join(__dirname, 'node_modules/startbootstrap-sb-admin-2')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);

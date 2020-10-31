4559ce0ea3a2c02dafe400beb181e6a04b2cb7dc
abdul23lm <abdul23lm@gmail.com>
2020-11-01 04:57:52 +0700

    Create activity, feature, and item schema models

---
+++ models/Item.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+const itemSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: String,
+        required: true
+    },
+    country: {
+        type: String,
+        default: 'Indonesia'
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    isPopular: {
+        type: Boolean
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    imageId: [{
+        type: ObjectId,
+        ref: 'Image'
+    }],
+    featureId: [{
+        type: ObjectId,
+        ref: 'Feature'
+    }],
+    activityId: [{
+        type: ObjectId,
+        ref: 'Acivity'
+    }]
+})
+
+module.exports = mongoose.model('Item', itemSchema)

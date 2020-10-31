5a12a889cc6a3d6037a829bda539c0dfc2433a5b
abdul23lm <abdul23lm@gmail.com>
2020-11-01 05:07:11 +0700

    Create booking, member, and bank scheme on models

---
+++ models/Bank.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const bankSchema = new mongoose.Schema({
+    nameBank: {
+        type: String,
+        required: true
+    },
+    nomorRekening: {
+        type: Number,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('Bank', bankSchema)

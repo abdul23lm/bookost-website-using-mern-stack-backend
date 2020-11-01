5692dc9ee628b72dbc8378a4301a3342490009d1
abdul23lm <abdul23lm@gmail.com>
2020-11-02 01:47:39 +0700

    Complete CRUD on category

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const methodOverride = require('method-override');
 
 // Import mongoose
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/myapp', {
+mongoose.connect('mongodb://localhost:27017/bookost', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
@@ -25,7 +26,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(methodOverride('_method'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,12 @@ const router = require('express').Router();
 const adminController = require('../controllers/adminController');
 
 router.get('/dashboard', adminController.viewDashboard);
+// endpoint category
 router.get('/category', adminController.viewCategory);
+router.post('/category', adminController.addCategory);
+router.put('/category', adminController.editCategory);
+router.delete('/category/:id', adminController.deleteCategory);
+
 router.get('/bank', adminController.viewBank);
 router.get('/item', adminController.viewItem);
 router.get('/booking', adminController.viewBooking);

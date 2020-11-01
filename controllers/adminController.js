a445b3c3e5b0f4aff4d15e58ba5368fc682b997e
abdul23lm <abdul23lm@gmail.com>
2020-11-01 18:28:58 +0700

    Add view bank

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -5,5 +5,9 @@ module.exports = {
 
     viewCategory: (req, res) => {
         res.render('admin/category/view_category');
+    },
+
+    viewBank: (req, res) => {
+        res.render('admin/bank/view_bank');
     }
 }
--- routes/admin.js
+++ routes/admin.js
@@ -3,5 +3,6 @@ const adminController = require('../controllers/adminController');
 
 router.get('/dashboard', adminController.viewDashboard);
 router.get('/category', adminController.viewCategory);
+router.get('/bank', adminController.viewBank);
 
 module.exports = router;

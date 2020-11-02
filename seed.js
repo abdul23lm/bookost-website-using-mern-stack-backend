98caccb95ceb24372d837be1e904e401dc8acbf8
abdul23lm <abdul23lm@gmail.com>
2020-11-03 05:14:07 +0700

    Fix some bugs on loading images

--- seed.js
+++ seed.js
@@ -1009,7 +1009,7 @@ var data = [
         memberId: mongoose.Types.ObjectId('5e96cbe292b97300fc903333'),
         bankId: mongoose.Types.ObjectId('5e96cbe292b97300fc903323'),
         payments: {
-          proofPayment: 'images/bukti.jpeg',
+          proofPayment: 'images/buktibayar.jpeg',
           bankFrom: 'BCA',
           status: 'Proses',
           accountHolder: 'ang'

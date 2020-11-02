f56e360e3f5d8411ea2ed48660887a8d21b227ac
abdul23lm <abdul23lm@gmail.com>
2020-11-02 07:40:13 +0700

    Complete CRUD on item

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,31 +1,6 @@
 const multer = require("multer");
 const path = require("path");
-const fs = require("fs");
-// import uuid from "uuid/v4";
 
-// const storageMultiple = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     var dir = 'public/images';
-//     if (!fs.existsSync(dir)) {
-//       fs.mkdirSync(dir);
-//     }
-//     cb(null, dir);
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, Date.now() + path.extname(file.originalname));
-//   }
-// })
-
-// const uploadMultiple = multer({
-//   storage: storageMultiple,
-//   limits: { fileSize: 1000000 },
-//   fileFilter: function (req, file, cb) {
-//     checkFileType(file, cb);
-//   }
-// }).array("image", 12);
-
-
-// Set storage engine
 const storage = multer.diskStorage({
   destination: "public/images",
   filename: function (req, file, cb) {
@@ -33,30 +8,30 @@ const storage = multer.diskStorage({
   }
 });
 
-const upload = multer({
+const uploadSingle = multer({
   storage: storage,
-  limits: { fileSize: 1000000 },
+  // limits: { fileSize: 1000000 },
   fileFilter: function (req, file, cb) {
     checkFileType(file, cb);
   }
 }).single("image");
 
-// // Check file Type
+const uploadMultiple = multer({
+  storage: storage,
+  // limits: { fileSize: 1000000 },
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  }
+}).array("image");
+
 function checkFileType(file, cb) {
-
-  // Allowed ext
   const fileTypes = /jpeg|jpg|png|gif/;
-  // Check ext
   const extName = fileTypes.test(path.extname(file.originalname).toLowerCase());
-  // Check mime
   const mimeType = fileTypes.test(file.mimetype);
-
   if (mimeType && extName) {
     return cb(null, true);
   } else {
     cb("Error: Images Only !!!");
   }
 }
-
-// module.exports = { uploadMultiple, upload };
-module.exports = { upload };
+module.exports = { uploadSingle, uploadMultiple };
--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const adminController = require('../controllers/adminController');
-const { upload } = require('../middlewares/multer');
+const { uploadSingle, uploadMultiple } = require('../middlewares/multer');
 
 router.get('/dashboard', adminController.viewDashboard);
 // endpoint category
@@ -10,10 +10,15 @@ router.put('/category', adminController.editCategory);
 router.delete('/category/:id', adminController.deleteCategory);
 // endpoint bank
 router.get('/bank', adminController.viewBank);
-router.post('/bank', upload, adminController.addBank);
-router.put('/bank', upload, adminController.editBank);
-router.delete('/bank/:id', adminController.deleteBank);
+router.post('/bank', uploadSingle, adminController.addBank);
+router.put('/bank', uploadSingle, adminController.editBank);
+// endpoint item
 router.get('/item', adminController.viewItem);
+router.post('/item', uploadMultiple, adminController.addItem);
+router.get('/item/show-image/:id', adminController.showImageItem);
+router.get('/item/:id', adminController.showEditItem);
+router.put('/item/:id', uploadMultiple, adminController.editItem);
+router.delete('/item/:id/delete', adminController.deleteItem);
 router.get('/booking', adminController.viewBooking);
 
 module.exports = router;

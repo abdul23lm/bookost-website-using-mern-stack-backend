4711017623091f48db90f3aae14ca8b0c2a94ef4
abdul23lm <abdul23lm@gmail.com>
2020-11-02 17:14:42 +0700

    Setup login page

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -4,15 +4,69 @@ const Item = require('../models/Item');
 const Image = require('../models/Image');
 const Feature = require('../models/Feature');
 const Activity = require('../models/Activity');
+const Users = require('../models/Users');
 const fs = require('fs-extra');
 const path = require('path');
+const bcrypt = require('bcryptjs')
 
 module.exports = {
     viewDashboard: (req, res) => {
         res.render('admin/dashboard/view_dashboard', {
             title: "BooKost | Dashboard"
         });
-    },
+  },
+  
+
+  viewSignin: async (req, res) => {
+    try {
+      const alertMessage = req.flash('alertMessage');
+      const alertStatus = req.flash('alertStatus');
+      const alert = { message: alertMessage, status: alertStatus };
+      if (req.session.user == null || req.session.user == undefined) {
+        res.render('index', {
+          alert,
+          title: "BooKost | Login"
+        });
+      } else {
+        res.redirect('/admin/dashboard');
+      }
+    } catch (error) {
+      res.redirect('/admin/signin');
+    }
+  },
+
+    actionSignin: async (req, res) => {
+    try {
+      const { username, password } = req.body;
+      const user = await Users.findOne({ username: username });
+      if (!user) {
+        req.flash('alertMessage', 'The user you entered does not exist !!');
+        req.flash('alertStatus', 'danger');
+        res.redirect('/admin/signin');
+      }
+      const isPasswordMatch = await bcrypt.compare(password, user.password);
+      if (!isPasswordMatch) {
+        req.flash('alertMessage', 'The password you entered doesnt match !!');
+        req.flash('alertStatus', 'danger');
+        res.redirect('/admin/signin');
+      }
+
+      req.session.user = {
+        id: user.id,
+        username: user.username
+      }
+
+      res.redirect('/admin/dashboard');
+
+    } catch (error) {
+      res.redirect('/admin/signin');
+    }
+  },
+
+  actionLogout: (req, res) => {
+    req.session.destroy();
+    res.redirect('/admin/signin');
+  },
 
     viewCategory: async (req, res) => {
         try {
